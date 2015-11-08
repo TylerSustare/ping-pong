0382cf9562ccfed1441af7ad8b2841859f01aee9
Tyler Sustare <tylersustare@gmail.com>
2015-11-08 15:46:40 -0800

    Finishes js funciton to play Ping-Pong game on button event with input. BUG: Still cannot get the main.js script into the page. Created separate 'sandbox' project with only input in the html file, only the main.js file and same jquery-1.11.3.js. main.js will load into the sandbox project, and the app works as expected. Could be where the script tag is in index.html. Further testing is required

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
-  $("button#play-me").click(function() {
-    $("ul#answer-me").text("TEST");
+	$("button#play-me").click(function() {
+		var userInput = $("input#txtinput").val();
+		var maxNum = Number(userInput);
+		$(".answers ul").empty()	// clear answers ul of items every time before showing answers
+		for (i = 1; i <= maxNum; ++i){
+			if (i%15 === 0){ // if divisible by 15
+				$("ul").append('<li>' + 'pingpong' + '</li>');
+			}
+			else if (i%5 === 0){ // if divisible by 5
+				$("ul").append('<li>' + 'pong' + '</li>');
+			}
+			else if (i%3 === 0){ // if divisible by 3
+				$("ul").append('<li>' + 'ping' + '</li>');
+			}
+			else if (true){ // if not divisible by 15, 5 or 3
+				$("ul").append('<li>' + i + '</li>');
+			}
+		}	// end for
   });
 });
