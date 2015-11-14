a992c651808f54c1114b26321e236642c56309e5
Tyler Sustare <tylersustare@gmail.com>
2015-11-14 10:49:51 -0800

    Updates scripts.js by removing redundant if statements in jquery section.

--- js/scripts.js
+++ js/scripts.js
@@ -19,18 +19,7 @@ $(document).ready(function(){
 		$(".answers ul").empty()	// clear answers ul of items every time before showing answers
 
 		for (var i = 1; i <= userInput; ++i){ // traditional loop variable 'i'
-			if (i%15 === 0){ // explicit if divisible by 15
 				$("ul").append('<li>' + pingPong(i) + '</li>');
-			}
-			else if (i%5 === 0){ // if divisible by 5
-				$("ul").append('<li>' + pingPong(i) + '</li>');
-			}
-			else if (i%3 === 0){ // if divisible by 3
-				$("ul").append('<li>' + pingPong(i) + '</li>');
-			}
-			else { // if i is not divisible by 15, 5 or 3
-				$("ul").append('<li>' + pingPong(i) + '</li>');
-			}
 		}	// end for
   });
 });
