77acc96fa75a7aee7d9dfcd4045251e5c6d9c066
Tyler Sustare <tylersustare@gmail.com>
2015-11-14 10:59:36 -0800

    Finishes spec.js tests for all integer values.

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,17 @@
 describe('pingPong', function(){
-  it("is pingpong for a number divisible by 15", function(){
+  it("return is 'pingpong' for a number divisible by 15", function(){
     expect(pingPong(15)).to.equal('pingpong');
+  });
+
+  it("return is 'pong' for a number divisible by 5", function(){
+    expect(pingPong(5)).to.equal('pong');
+  });
+
+  it("return is 'ping' for a number divisible by 3", function(){
+    expect(pingPong(3)).to.equal('ping');
+  });
+
+  it("return is just the number passed in for all other numbers", function(){
+    expect(pingPong(2)).to.equal(2);
   });
 });
